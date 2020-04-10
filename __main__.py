f3d0fa899646a9f3d05134e5f4be0bbd32ff99ad
Alex Krol <krolmail@list.ru>
2020-04-10 23:15:36 +0300

    One more branch
    Making good interface

--- __main__.py
+++ __main__.py
@@ -2,70 +2,88 @@ import wx
 
 
 #####################################################################
+# onToolbarClick
+#####################################################################
+def onToolbarClick(self, event):
+    id = event.getId()
+    pass
+
+
+#####################################################################
 # wx.Frame MainFrame
 #####################################################################
 class MainFrame(wx.Frame):
+    file_filters = "AVI (*.avi)|*.avi|All files (*.*)|*.*"
+
     def __init__(self, *args, **kwds):
         kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
-        self.SetSize((492, 320))
+        self.SetSize((576, 432))
 
         # <ToolBar>
-        self.frame_toolbar = wx.ToolBar(self, -1)
-        self.SetToolBar(self.frame_toolbar)
-        self.frame_toolbar.AddTool(1000, r"Открыть видео",
-                                    wx.Bitmap("icons\\open.png", wx.BITMAP_TYPE_ANY),
-                                    wx.Bitmap("icons\\open.disabled.png", wx.BITMAP_TYPE_ANY),
-                                    wx.ITEM_NORMAL, r"Открыть видео", "")
-        self.frame_toolbar.AddTool(1001, r"Сохранить видео",
-                                   wx.Bitmap("icons\\save.png", wx.BITMAP_TYPE_ANY),
-                                   wx.Bitmap("icons\\save.disabled.png", wx.BITMAP_TYPE_ANY),
-                                   wx.ITEM_NORMAL, r"Сохранить видео", "")
-        self.frame_toolbar.AddSeparator()
-        self.frame_toolbar.AddTool(1010, r"Отменить",
-                                   wx.Bitmap("icons\\undo.png", wx.BITMAP_TYPE_ANY),
-                                   wx.Bitmap("icons\\undo.disabled.png", wx.BITMAP_TYPE_ANY),
-                                   wx.ITEM_NORMAL, r"Отменить", "")
-        self.frame_toolbar.AddTool(1011, r"Повторить",
-                                   wx.Bitmap("icons\\repeat.png", wx.BITMAP_TYPE_ANY),
-                                   wx.Bitmap("icons\\repeat.disabled.png", wx.BITMAP_TYPE_ANY),
-                                   wx.ITEM_NORMAL, r"Повторить", "")
-        self.frame_toolbar.AddSeparator()
-        self.frame_toolbar.AddTool(1020, r"Новый эффект",
-                                   wx.Bitmap("icons\\effect.png", wx.BITMAP_TYPE_ANY),
-                                   wx.Bitmap("icons\\effect.disabled.png", wx.BITMAP_TYPE_ANY),
-                                   wx.ITEM_NORMAL, r"Новый эффект", "")
+        self.toolbar = wx.ToolBar(self, -1, style=wx.TB_DEFAULT_STYLE | wx.TB_TEXT)
+        self.SetToolBar(self.toolbar)
+        self.toolbar.AddTool(wx.ID_OPEN, r"Открыть видео",
+                             wx.Bitmap("icons\\open.png", wx.BITMAP_TYPE_ANY),
+                             wx.Bitmap("icons\\open.disabled.png", wx.BITMAP_TYPE_ANY),
+                             wx.ITEM_NORMAL, r"Открыть видео", "")
+        self.toolbar.AddTool(wx.ID_SAVE, r"Сохранить видео",
+                             wx.Bitmap("icons\\save.png", wx.BITMAP_TYPE_ANY),
+                             wx.Bitmap("icons\\save.disabled.png", wx.BITMAP_TYPE_ANY),
+                             wx.ITEM_NORMAL, r"Сохранить видео", "")
+        self.toolbar.AddSeparator()
+        self.toolbar.AddTool(wx.ID_UNDO, r"Отменить",
+                             wx.Bitmap("icons\\undo.png", wx.BITMAP_TYPE_ANY),
+                             wx.Bitmap("icons\\undo.disabled.png", wx.BITMAP_TYPE_ANY),
+                             wx.ITEM_NORMAL, r"Отменить", "")
+        self.toolbar.AddTool(wx.ID_REDO, r"Повторить",
+                             wx.Bitmap("icons\\repeat.png", wx.BITMAP_TYPE_ANY),
+                             wx.Bitmap("icons\\repeat.disabled.png", wx.BITMAP_TYPE_ANY),
+                             wx.ITEM_NORMAL, r"Повторить", "")
+        self.toolbar.AddSeparator()
+        self.toolbar.AddTool(wx.ID_ADD, r"Новый эффект",
+                             wx.Bitmap("icons\\effect.png", wx.BITMAP_TYPE_ANY),
+                             wx.Bitmap("icons\\effect.disabled.png", wx.BITMAP_TYPE_ANY),
+                             wx.ITEM_NORMAL, r"Новый эффект", "")
+        self.toolbar.EnableTool(wx.ID_SAVE, False)
+        self.toolbar.EnableTool(wx.ID_UNDO, False)
+        self.toolbar.EnableTool(wx.ID_REDO, False)
+        self.toolbar.EnableTool(wx.ID_ADD, False)
         # </ToolBar>
 
         self.__set_properties()
         self.__do_layout()
 
-        self.Bind(wx.EVT_TOOL, self.click_open, id=1000)
-        self.Bind(wx.EVT_TOOL, self.click_save, id=1001)
-        self.Bind(wx.EVT_TOOL, self.click_undo, id=1010)
-        self.Bind(wx.EVT_TOOL, self.click_repeat, id=1011)
-        self.Bind(wx.EVT_TOOL, self.click_addeffect, id=1020)
+        self.Bind(wx.EVT_TOOL, self.click_open, id=wx.ID_OPEN)
+        self.Bind(wx.EVT_TOOL, self.click_save, id=wx.ID_SAVE)
+        self.Bind(wx.EVT_TOOL, self.click_undo, id=wx.ID_UNDO)
+        self.Bind(wx.EVT_TOOL, self.click_redo, id=wx.ID_REDO)
+        self.Bind(wx.EVT_TOOL, self.click_addeffect, id=wx.ID_ADD)
 
     def __set_properties(self):
         self.SetTitle(r"PyVideo")
-        self.frame_toolbar.Realize()
+        self.toolbar.Realize()
 
     def __do_layout(self):
         self.Layout()
 
     def click_open(self, event):
-        print("Event handler 'click_open' not implemented!")
-        event.Skip()
+        dialog = wx.FileDialog(self, message="Select video file...", defaultDir="",
+                               wildcard=self.file_filters, style=wx.FD_OPEN)
+        if dialog.ShowModal() == wx.ID_OK:
+            pass # self.load_file(dialog.GetPath())
 
     def click_save(self, event):
-        print("Event handler 'click_save' not implemented!")
-        event.Skip()
+        dialog = wx.FileDialog(self, message="Save as...", defaultDir="",
+                               wildcard=self.file_filters, style=wx.FD_SAVE)
+        if dialog.ShowModal() == wx.ID_OK:
+            pass
 
     def click_undo(self, event):
         print("Event handler 'click_undo' not implemented!")
         event.Skip()
 
-    def click_repeat(self, event):
+    def click_redo(self, event):
         print("Event handler 'click_repeat' not implemented!")
         event.Skip()
 
